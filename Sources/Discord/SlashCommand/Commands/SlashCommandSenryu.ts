3ecd8994e82e421ecc22828909e23766ff27c589
PImmediately <48210492+PImmediately@users.noreply.github.com>
2026-04-09 16:48:13 +0900

    Add a command to send the occurrence counts of senryu phrases

--- Sources/Discord/SlashCommand/Commands/SlashCommandSenryu.ts
+++ Sources/Discord/SlashCommand/Commands/SlashCommandSenryu.ts
@@ -2,10 +2,12 @@ import Discord from "discord.js";
 import SlashCommand from "./SlashCommand";
 
 import SlashCommandSenryuRank from "./SlashCommandSenryuRank";
+import SlashCommandSenryuDictionary from "./SlashCommandSenryuDictionary";
 
 export default class SlashCommandSenryu extends SlashCommand {
 
 	private readonly senryuRank = new SlashCommandSenryuRank();
+	private readonly senryuDictionary = new SlashCommandSenryuDictionary();
 
 	override readonly command = new Discord.SlashCommandBuilder()
 		.setName("senryu")
@@ -14,12 +16,21 @@ export default class SlashCommandSenryu extends SlashCommand {
 			return group
 				.setName("rank")
 				.setDescription("川柳ランキングを送信します。");
+		})
+		.addSubcommand((group) => {
+			return group
+				.setName("dictionary")
+				.setDescription("句の出現回数を送信します。");
 		});
 
 	override onExecute(interaction: Discord.ChatInputCommandInteraction<Discord.CacheType>): void {
 		switch (interaction.options.getSubcommand()) {
 			case "rank": {
 				this.senryuRank.onExecute(interaction);
+				return;
+			}
+			case "dictionary": {
+				this.senryuDictionary.onExecute(interaction);
 				return;
 			}
 		}
---
+++ Sources/Discord/SlashCommand/Commands/SlashCommandSenryuDictionary.ts
@@ -0,0 +1,55 @@
+import Discord from "discord.js";
+import SlashCommand from "./SlashCommand";
+
+export default class SlashCommandSenryuDictionary extends SlashCommand {
+
+	override readonly command = undefined;
+
+	override async onExecute(interaction: Discord.ChatInputCommandInteraction<Discord.CacheType>): Promise<void> {
+		if (!interaction.inCachedGuild()) return;
+
+		const databaseSenryu = interaction.client.discordBOT.app.databaseSenryu;
+
+		const contentsEachRule: Record<string, string[]> = {};
+		databaseSenryu.forEachSenryu(interaction.guildId, (senryu) => {
+			if (!senryu.author) return;
+			for (let i: number = 0; i < senryu.content.length; i++) {
+				const rule = String(senryu.rule[i]!);
+				const content = senryu.content[i]!;
+				if (!contentsEachRule[rule]) contentsEachRule[rule] = new Array<string>();
+				contentsEachRule[rule].push(content);
+			}
+		});
+		if (Object.keys(contentsEachRule).length === 0) {
+			await interaction.reply("無の川柳を観測");
+			return;
+		}
+
+		await interaction.reply({
+			embeds: [
+				new Discord.EmbedBuilder()
+					.setTitle("句の出現回数")
+					.setDescription(
+						Object.entries(contentsEachRule).map(([rule, contents]) => {
+							const countEachContent: Record<string, number> = {};
+							contents.forEach((content) => {
+								if (!countEachContent[content]) countEachContent[content] = 0;
+								countEachContent[content]++;
+							});
+
+							const ranking = Object.entries(countEachContent)
+								.sort((a, b) => b[1] - a[1])
+								.slice(0, 10)
+								.map(([content, count], index) => {
+									return `**${index + 1}位** ${content}（${count}回）`;
+								})
+								.join("\n");
+
+							return `### ${rule}音\n${ranking}`;
+						}).join("\n")
+					)
+			]
+		});
+	}
+
+}
